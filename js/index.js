96d0de79b350a2d6f64b5cce6bc88e6c7012f77d
unknown <agomcago@gmail.com>
2025-06-10 07:55:16 +0100

    sort by the number of likes

--- js/index.js
+++ js/index.js
@@ -4,8 +4,16 @@
 const postsContainer = document.querySelector(".blogs");
 
 const renderPosts = async () => {
-  
-    let postsUri = "http://localhost:3000/posts"; // store the post uri (this will normally be hidden in a database connection string).
+    /*
+    parameters passed into the uri can be used to sort the data, by any of the properties (likes, title, body); the default is by id.
+
+    And the default sorting order is from the lowest to the highest, meaning post with id of 1 will be at the top of the page, or
+    post with the lowest likes will be at the top of the page.
+
+    to reverse the default and make it so post with higher likes be top of the page, "&_order=desc" is used; meaning sort in
+    descending order, "&" is used to join/add another parameter.
+    */
+    let postsUri = "http://localhost:3000/posts?_sort=likes&_order=desc";
 
     const response = await fetch(postsUri);
     const posts = await response.json();
